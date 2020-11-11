72e5f6e2973bdc3099fc094403b461a50819a50d
Hemant Wagh <hemantwagh07@gmail.com>
2020-11-11 18:20:54 +0530

    Added json server and autocomplete
    Added json server and autocomplete

---
+++ src/Components/Search/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTxt: '',
+            searchResults: []
+        }
+    }
+    getSuggestion(search) {
+        axios.get(`http://localhost:8000/posts?title_like=${search}`)
+            .then((response) => {
+                this.setState({ searchResults: response.data });
+            }).catch(function (error) {
+                console.error('Something went wrong retrieving list. Please try again', error);
+            })
+    }
+
+    changeHandler = (e) => {
+        let txtval = e.target.value;
+        if (txtval !== "") {
+            this.getSuggestion(txtval);
+        } else {
+            this.setState({ searchResults: [] });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h5>Search User</h5>
+                <div>
+                    <input type="text" name="userName" onChange={this.changeHandler} />
+                    {this.state.searchResults.map((result) => (
+                        <div key={Math.random(100)}>{result.title}</div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
